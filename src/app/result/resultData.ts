901c6404251295d1ca97ee7464a87bd70417d9a2
speardragon <rkdckdfyyd@naver.com>
2024-03-06 17:31:18 +0900

    🍱 chore: 분석 결과 이미지 및 데이터

---
+++ src/app/result/resultData.ts
@@ -0,0 +1,62 @@
+export const readingTypeInfo = {
+  "0": {
+    type: "종합 탐색가",
+    image: "/images/general.jpg",
+    analysis:
+      "다양한 주제에 호기심이 많고, 새로운 지식을 적극적으로 탐색하는 성향. 백과사전, 종합 서적 등에 관심이 많음.",
+  },
+  "1": {
+    type: "철학적 사색가",
+    image: "/images/philosophy.jpg",
+    analysis:
+      "심오한 사고를 즐기며, 철학적 질문과 사색에 몰두하는 성향. 심리학, 윤리학, 논리학 등에 깊은 흥미를 보임.",
+  },
+  "2": {
+    type: "영적 탐구자",
+    image: "/images/religion.jpg",
+    analysis:
+      "종교적, 영적 가치에 깊은 관심을 가지고, 신앙과 관련된 지식을 탐구하는 성향. 성서, 종교 철학 등에 몰두함.",
+  },
+  "3": {
+    type: "사회적 현실주의자",
+    image: "/images/social_science.jpg",
+    analysis:
+      "사회적 이슈와 현상에 대해 관심이 많고, 경제, 법률, 교육, 사회 문제 등에 대한 심도 있는 탐색을 추구함.",
+  },
+  "4": {
+    type: "자연 탐험가",
+    image: "/images/natural_science.jpg",
+    analysis:
+      "자연 세계와 우주에 대한 호기심이 강하며, 수학, 천문학, 물리학 등 자연과학 분야에 끌림.",
+  },
+  "5": {
+    type: "기술 혁신가",
+    image: "/images/technology.jpg",
+    analysis:
+      "새로운 기술과 과학적 발명에 열정적이며, 의학, 공학, 농업 등의 최신 동향과 혁신에 관심이 많음.",
+  },
+  "6": {
+    type: "예술적 창조자",
+    image: "/images/art.png",
+    analysis:
+      "예술적 감성이 풍부하며, 조각, 회화, 음악, 무용 등 다양한 예술 형태에 대한 깊은 이해와 흥미를 가짐. 창조적 표현과 예술적 아름다움에 감동을 받는 성향.",
+  },
+  "7": {
+    type: "언어 마스터",
+    image: "/images/language.jpg",
+    analysis:
+      "다양한 언어와 문화에 관심이 많으며, 한국어, 영어, 독일어 등 여러 언어를 탐구하고 학습하는 데 열정적임.",
+  },
+  "8": {
+    type: "문학적 꿈꾸는이",
+    image: "/images/literature.jpg",
+    analysis:
+      "문학 작품에 몰입하여 감정과 상상력을 키우며, 한국 문학, 영미 문학 등 다양한 장르와 스타일의 문학 작품을 탐닉함.",
+  },
+  "9": {
+    type: "역사적 탐험가",
+    image: "/images/history.jpg",
+    analysis:
+      "과거의 사건과 문화에 대한 깊은 관심을 가지고, 아시아 역사, 유럽 역사, 지리, 전기 등을 통해 인류의 역사를 탐구함.",
+  },
+};
